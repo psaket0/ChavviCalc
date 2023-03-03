14578be2e7d3227f56cb1fb766ed9e1a2379ef7f
Saket Pathak <99856959+psaket0@users.noreply.github.com>
2023-03-02 19:40:30 -0800

    Update App.java

--- App.java
+++ App.java
@@ -1,94 +1,131 @@
+
 import java.text.DecimalFormat;
 import java.util.Scanner;
-import java.util.*;
+public class App
+{
+    public static void main( String[] args ){
 
-/*
- * chavvi calc calculator
- */
-public class App {
-
-  public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    Character command = '_';
-
-   
-    double rawA =0;
-    double rawB =0;
-    String displayA, displayB;
+    double a =0;
+    double b =0;
+    String homeA, homeB;
     String choice;
 
-   
+    System.out.println ("\n------------------------------------------------------------------------------");
+    System.out.println("Chavvi Calculator:");
     DecimalFormat _numberFormat= new DecimalFormat("#0.000");
-    displayA = _numberFormat.format(rawA);
-    displayB = _numberFormat.format(rawB);
-
-  
-
+    homeA = _numberFormat.format(a);
+    homeB = _numberFormat.format(b);
         
 
-    // loop until user quits
-    while (command != 'q') {
-      printMenu();
-      System.out.print("Enter a command: ");
-      command = menuGetCommand(scan);
+//public static void menu(){
+        System.out.println("\na \t enter a value for A");
+        System.out.println("b \t enter a value for B");
+        System.out.println("+ \t add");
+        System.out.println("- \t subtract");
+        System.out.println("* \t multiply");
+        System.out.println("/ \t divide");
+        System.out.println("c \t clear ");
+        System.out.println("q \t quit \n");
+        System.out.println ("------------------------------------------------------------------------------");
+    
 
-      executeCommand(scan, command);
+    programs:
+    while (true) {
+        homeA = _numberFormat.format(a);
+        homeB = _numberFormat.format(b);
+        System.out.println ("------------------------------------------------------------------------------");
+        System.out.println(String.format("A = %s \t B = %s ",homeA, homeB));
+        System.out.println ("------------------------------------------------------------------------------");
+        //home;
+        System.out.print("Enter your choice : ");
+        choice = scan.next();
+
+        switches:
+        switch (choice) {
+            case "a":
+                a = A(scan);
+                System.out.println("\n");
+                continue programs;
+            case "b":
+                b = B(scan);
+                System.out.println("\n");
+                break switches;
+            case "+":
+                a = add(a, b);
+                System.out.println("\n");
+                break switches;
+            case "-":
+                a = subtract(a, b);
+                System.out.println("\n");
+                break switches;
+            case "*":
+                a = multiply(a, b);
+                System.out.println("\n");
+                break switches;
+            case "/":
+            if (a == 0) {
+                System.out.println("please change b to another number which is not 0");
+                break switches;
+            }
+                a = divide(a, b);
+                System.out.println("\n");
+                break switches;
+            case "c":
+                System.out.println("\n");
+                a = 0;
+               b = 0;
+                break switches;
+            case "q":
+            System.out.println("\n");
+                break programs;
+            default:
+                System.out.println("Invalid choice");
+                break switches;
+            }
+            System.out.println("A becomes your output when calculated! You can then hit an operation to perform b on a!");
+
+        
+        System.out.println("Thank you for using the chavviCalculator \n"); }
+        
+    }
+    public static double A (Scanner scanner){
+        System.out.print("Enter A : ");
+        return getNum(scanner);
+        
+    }
+    public static double B (Scanner scanner){
+        System.out.print("Enter B : ");
+        return getNum(scanner);
+    }
+    
+    public static double add (double rawA, double rawB){
+        return rawA+rawB;
+    }
+    public static double subtract (double rawA, double rawB){
+        return rawA-rawB;
+    }
+    public static double multiply(double rawA, double rawB){
+        return rawA*rawB;
+    }
+    public static double divide(double rawA, double rawB){
+        return rawA/rawB;
     }
 
-    scan.close();
-  }
+    public static double getNum(Scanner scanner){
+        String r;
+        double Num;
+        while (true){
+            r = scanner.next();
 
-  //
-  // menu functions
-  //
-  private static void printMenuLine() {
-    System.out.println(
-      "----------------------------------------------------------"
-    );
-  }
-
-  private static void printMenuCommand(Character command, String desc) {
-    System.out.printf("%s\t%s\n", command, desc);
-  }
-
-  // prints the menu
-  public static void printMenu() {
-    printMenuLine();
-    System.out.println("ChavviCalc");
-    printMenuLine();
-
-    printMenuCommand('q', "Quit");
-
-    printMenuLine();
-  }
-
-  // get first character from input
-  private static Character menuGetCommand(Scanner scan) {
-    Character command = '_';
-
-    String rawInput = scan.nextLine();
-
-    if (rawInput.length() > 0) {
-      rawInput = rawInput.toLowerCase();
-      command = rawInput.charAt(0);
-    }
-
-    return command;
-  }
-
-  // calculator functions
-  private static Boolean executeCommand(Scanner scan, Character command) {
-    Boolean success = true;
-
-    switch (command) {
-      case 'q':
-        System.out.println("Thank you for using Chavvi Calc");
-        break;
-      default:
-        System.out.println("ERROR: Unknown commmand");
-        success = false;
-    }
-
-    return success;
-  }
-}
+            try {
+            Num= Double.parseDouble(r);
+          }catch (NumberFormatException e) {
+            System.out.println("Enter a Number not a string!");
+            continue;
+            }
+            break;
+        }
+        return Num;
+    
+}}
